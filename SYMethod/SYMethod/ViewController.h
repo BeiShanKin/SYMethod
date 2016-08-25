c1a963c4166e2b289ef27a4e9f30b1b7bfaa5e79
songyu <syiosengineer1992@gmail.com>
2016-08-25 22:05:08 +0800

    初始化

---
+++ SYMethod/SYMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYMethod
+//
+//  Created by lz on 16/8/25.
+//  Copyright © 2016年 SY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
